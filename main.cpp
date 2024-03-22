163d4fa1613a26cf4739795b92886d7914deab25
lgalvanl <lgalvanl@fi.uba.ar>
2024-03-22 00:46:52 +0000

    1er commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
+#include <cstdio>
+// 21/3/2024
+// Sistemas embebidos 
 
 int main()
 {
@@ -10,7 +13,9 @@ int main()
     gasDetector.mode(PullDown);
 
     alarmLed = OFF;
-
+    printf("%s\n", "Hola somos el grupo 2748");
+    printf("%s\n", "Lautaro De Lucia - 100203");
+    printf("%s\n", "Ludmila Galvan Lopez -105975");
     while (true) {
         if ( gasDetector == ON ) {
             alarmLed = ON;
